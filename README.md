f978311de135010b64390a31e737460513908602
DianacraftGaming <lifehack4you.diana@gmail.com>
2025-04-24 21:21:55 +0200

    Got the command working, updated readme a bit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# lifeskins
-An automatic skin changer depenting on the life amount for the Life Series mod!
+# Life Skins
+A mod for changing player skins depending on the life amount without needing to restart  Minecraft! Requires the [Life Series](https://modrinth.com/mod/life-series/) mod for lives tracking! Also requires [Fabric Tailor](https://modrinth.com/mod/fabrictailor/) to run properly (Hoping to make it optional in a future update)
+
+## How to use
+Running the /skin reload command will update your skin 
+## How to set up
+### Advanced
+## Extra info
+I plan to mainly release versions I myself am going to play on, so I will most likely skip a few. If there's a particular version you'd like me to release a mod version for, feel free to contact me either on Discord(my username is dianacraft) or by creating an issue on [Github](https://github.com/DianacraftGaming/lifeskins/issues) and I'll see if I can get that done for you
+Thanks for enjoying the mod!
--- src/main/java/net/dianacraft/lifeskins/command/SkinReloadCommand.java
+++ src/main/java/net/dianacraft/lifeskins/command/SkinReloadCommand.java
@@ -1,21 +1,36 @@
 package net.dianacraft.lifeskins.command;
 
 import com.mojang.brigadier.CommandDispatcher;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.*;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.*;
+import org.samo_lego.fabrictailor.command.SkinCommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import static org.samo_lego.fabrictailor.util.SkinFetcher.setSkinFromFile;
 import static net.minecraft.server.command.CommandManager.*;
+
 public class SkinReloadCommand {
+    public static final String MOD_ID = "lifeskins";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
         dispatcher.register(
             literal("skin")
             .then(literal("reload")
                 .executes(context -> {
-                    context.getSource().sendFeedback(() -> Text.literal("Reloading the skin..."), false);
-                    //TODO: Figure out how the skin paths work
-                    //TODO: Possibly downgrade fabrictailor, as it crashes when i open the menu
-                    return 1;}
+                    //context.getSource().sendFeedback(() -> Text.literal("Reloading the skin..."), false);
+                    ServerPlayerEntity player = context.getSource().getPlayerOrThrow();
+                    String skinFilePath = "config/lifeskins/Player/2.png";
+                    SkinCommand.setSkin(player, () -> setSkinFromFile(skinFilePath, false));
+
+                    LOGGER.info("supposedly ran the command");
+                    //TODO: /skin set upload classic config/lifeskins/Player/2.png
+                    return 1;
+                    }
                 )
             )
         );
