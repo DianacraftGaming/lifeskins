bce9ded8349e5848cf57e255398e1206f642a7d5
DianacraftGaming <lifehack4you.diana@gmail.com>
2025-09-16 19:28:41 +0200

    fixed a mixing not gettting pushed

---
+++ src/main/java/net/dianacraft/lifeskins/mixin/ServerPlayerEntityMixin.java
@@ -0,0 +1,29 @@
+package net.dianacraft.lifeskins.mixin;
+
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import static net.dianacraft.lifeskins.command.LifeSkinsCommand.getLivesForLimited;
+import static net.dianacraft.lifeskins.command.LifeSkinsCommand.reloadSkin;
+import static net.mat0u5.lifeseries.Main.currentSeason;
+import static net.mat0u5.lifeseries.seasons.season.Seasons.LIMITED_LIFE;
+
+@Mixin(ServerPlayerEntity.class)
+public class ServerPlayerEntityMixin {
+    @Inject(method = "onSpawn", at = @At("HEAD"))
+    public void onSpawn(CallbackInfo ci) {
+        ServerPlayerEntity player = (ServerPlayerEntity)(Object)this;
+        try {
+            if (currentSeason.getSeason() == LIMITED_LIFE){
+                reloadSkin(player, getLivesForLimited(player));
+            } else {
+                reloadSkin(player);
+            }
+        } catch (CommandSyntaxException ignored) {}
+    }
+}
