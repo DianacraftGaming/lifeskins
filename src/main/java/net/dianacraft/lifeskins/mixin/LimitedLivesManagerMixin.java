ed95ae14266b936222f5047cc16b9f4f80ccdf0f
DianacraftGaming <lifehack4you.diana@gmail.com>
2025-09-17 18:15:41 +0200

    actually fixed stuff

--- src/main/java/net/dianacraft/lifeskins/mixin/LimitedLivesManagerMixin.java
+++ src/main/java/net/dianacraft/lifeskins/mixin/LimitedLivesManagerMixin.java
@@ -1,55 +1,33 @@
 package net.dianacraft.lifeskins.mixin;
 
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
-import net.dianacraft.lifeskins.util.SkinPathFinder;
 import net.mat0u5.lifeseries.seasons.other.LivesManager;
 import net.mat0u5.lifeseries.seasons.season.Seasons;
+import net.mat0u5.lifeseries.seasons.season.limitedlife.LimitedLifeLivesManager;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.text.Text;
 import org.jetbrains.annotations.Nullable;
-import org.samo_lego.fabrictailor.command.SkinCommand;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import static net.dianacraft.lifeskins.LifeSkins.LOGGER;
 import static net.dianacraft.lifeskins.command.LifeSkinsCommand.getLivesForLimited;
 import static net.dianacraft.lifeskins.command.LifeSkinsCommand.reloadSkin;
 import static net.mat0u5.lifeseries.Main.currentSeason;
 import static net.mat0u5.lifeseries.seasons.other.WatcherManager.isWatcher;
-import static org.samo_lego.fabrictailor.util.SkinFetcher.fetchSkinByName;
 
-@Mixin(LivesManager.class)
-public abstract class LivesManagerMixin {
-
-    @Shadow public abstract @Nullable Integer getPlayerLives(ServerPlayerEntity player);
-
-    @Inject(method = "resetPlayerLife", at = @At("HEAD"))
-    private void resetPlayerLife(ServerPlayerEntity player, CallbackInfo ci) {
-        SkinPathFinder spf = new SkinPathFinder(player);
-        if (spf.hasSkins() && player.isAlive()){
-            //File f = new File(spf.getDirectoryPath()+"/default.png");
-            //if(f.isFile()){
-            //    SkinCommand.setSkin(player, () -> setSkinFromFile(spf.getDirectoryPath()+"/default.png", SkinPathFinder.getSlim(spf.getSkin(Main.currentSeries.getPlayerLives(player)))));
-            //} else {
-            SkinCommand.setSkin(player, () -> fetchSkinByName(player.getName().getString()));
-            //}
-        }
-
-    }
+@Mixin(LimitedLifeLivesManager.class)
+public abstract class LimitedLivesManagerMixin extends LivesManager {
 
     @Inject(method = "setPlayerLives", at = @At("HEAD"))
     public void setPlayerLives(ServerPlayerEntity player, int lives, CallbackInfo ci){
-
         if (player == null) return;
         if (isWatcher(player)) return;
         int prevLives = -1;
         if (getPlayerLives(player) != null) prevLives = getPlayerLives(player);
-        if (lives != prevLives){
+        if (getLivesForLimited(lives) != getLivesForLimited(prevLives)){
             try {
-                reloadSkin(player, lives);
+                reloadSkin(player, getLivesForLimited(lives));
             } catch (CommandSyntaxException ignored) {}
         }
     }
--- src/main/java/net/dianacraft/lifeskins/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/net/dianacraft/lifeskins/mixin/ServerPlayerEntityMixin.java
@@ -1,6 +1,7 @@
 package net.dianacraft.lifeskins.mixin;
 
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import net.mat0u5.lifeseries.seasons.season.Seasons;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,13 +12,17 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import static net.dianacraft.lifeskins.command.LifeSkinsCommand.getLivesForLimited;
 import static net.dianacraft.lifeskins.command.LifeSkinsCommand.reloadSkin;
 import static net.mat0u5.lifeseries.Main.currentSeason;
+import static net.mat0u5.lifeseries.Main.livesManager;
 import static net.mat0u5.lifeseries.seasons.season.Seasons.LIMITED_LIFE;
 
 @Mixin(ServerPlayerEntity.class)
 public class ServerPlayerEntityMixin {
     @Inject(method = "onSpawn", at = @At("HEAD"))
     public void onSpawn(CallbackInfo ci) {
+        if (currentSeason.getSeason() == Seasons.UNASSIGNED) return;
         ServerPlayerEntity player = (ServerPlayerEntity)(Object)this;
+        if (!livesManager.hasAssignedLives(player)) return;
+
         try {
             if (currentSeason.getSeason() == LIMITED_LIFE){
                 reloadSkin(player, getLivesForLimited(player));
